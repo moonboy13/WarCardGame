930ce74f9cf06c4c8a9bbe0557b02ff7e139a0da
Kyle Conlon <conlonkyle@gmail.com>
2017-04-28 18:50:18 -0400

    Added a basic player class.

---
+++ CardGameWare/CardGameWare/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameWar
+{
+    class Player
+    {
+        private string name;
+
+        private string GetName()
+        {
+            return name;
+        }
+
+        private void SetName(string value)
+        {
+            name = value;
+        }
+
+        private List<Card> hand;
+    }
+}
